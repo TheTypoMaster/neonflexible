bd9f554d388c1766f3b60cc9010789aecdddc99c
Lefèvre Loic <contact@ninja-of-web.fr>
2014-09-06 20:59:43 +0200

    [MODULE] Suppression du module "now_block_footer"

--- modules/now_block_footer/translations/fr.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-global $_MODULE;
-$_MODULE = array();
-$_MODULE['<{now_block_footer}prestashop>now_block_footer_fd85d666d4c8659b7828eb3596274e2d'] = 'Bloc footer : catégorie, métier, information client';
-$_MODULE['<{now_block_footer}prestashop>now_block_footer_b47e2132dddae8539a70ccdfbea40ee8'] = 'Ajout d\'un bloc qui sépare en 3 colonnes les catégories, les métiers et les pages d\'information clients';
-$_MODULE['<{now_block_footer}prestashop>now_block_footer_72c57187d13c4be687f5050f6c971eae'] = 'Gestion du footer';
