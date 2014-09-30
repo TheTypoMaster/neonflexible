9f5b8e5e26b12b5a67f59f2c5c61a69a646b0f06
Lefèvre Loic <contact@ninja-of-web.fr>
2014-09-30 23:15:17 +0200

    Module now_slideshow version 1.0

---
+++ modules/now_slideshow/controllers/admin/AdminSlideshow.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * 2014
+ * Author: LEFEVRE LOIC
+ * Site: www.ninja-of-web.fr
+ * Mail: contact@ninja-of-web.fr
+ */
+
+include (_PS_MODULE_DIR_.'now_slideshow/now_slideshow.php');
+
+class AdminSlideshowController extends ModuleAdminController {
+	public $module;
+
+	public function __construct()
+	{
+		$this->module = new now_slideshow();
+
+		parent::__construct();
+	}
+}
---
+++ themes/neon-flexible-bootstrap/modules/now_slideshow/translations/fr.php
@@ -0,0 +1,6 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{now_slideshow}neon-flexible-bootstrap>now_slideshow_de9eceee2c7113c63bf2b1d023fcbf7d'] = 'Gestion des slides';
+$_MODULE['<{now_slideshow}neon-flexible-bootstrap>now_slideshow_e026daaf0daa95496bc874b006c0e83d'] = 'Gestion des slides de la home page';
