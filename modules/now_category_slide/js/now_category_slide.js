87ecec4a98a51367042c1f8fe5261794a16e192a
Lefèvre Loic <contact@ninja-of-web.fr>
2014-10-06 19:56:44 +0200

    Modification nombre de slide des métiers

--- modules/now_category_slide/js/now_category_slide.js
+++ modules/now_category_slide/js/now_category_slide.js
@@ -3,13 +3,13 @@ $('.carousel-slick-category').slick({
 	dots: false,
 	infinite: true,
 	speed: 300,
-	slidesToShow: 9,
+	slidesToShow: 8,
 	slidesToScroll: 1,
 	responsive: [
 		{
 			breakpoint: 1024,
 			settings: {
-				slidesToShow: 7,
+				slidesToShow: 6,
 				slidesToScroll: 1,
 				infinite: true,
 				dots: true
@@ -18,14 +18,14 @@ $('.carousel-slick-category').slick({
 		{
 			breakpoint: 600,
 			settings: {
-				slidesToShow: 5,
+				slidesToShow: 4,
 				slidesToScroll: 1
 			}
 		},
 		{
 			breakpoint: 480,
 			settings: {
-				slidesToShow: 3,
+				slidesToShow: 2,
 				slidesToScroll: 1
 			}
 		}
