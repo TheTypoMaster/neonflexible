f843a35ab710df4def31292858305d983d9d777e
Lefevre Loic <contact@ninja-of-web.fr>
2015-09-09 20:39:04 +0200

    MAJ du module blocksocial

--- modules/blocksocial/blocksocial.php
+++ modules/blocksocial/blocksocial.php
@@ -115,6 +115,23 @@ class blocksocial extends Module
 		return $this->display(__FILE__, 'blocksocial.tpl', $this->getCacheId());
 	}
 	
+	public function hookDisplayRightColumn($params)
+	{
+		if (!$this->isCached('blocksocial.tpl', $this->getCacheId()))
+			$this->smarty->assign(array(
+				'facebook_url' => Configuration::get('BLOCKSOCIAL_FACEBOOK'),
+				'twitter_url' => Configuration::get('BLOCKSOCIAL_TWITTER'),
+				'rss_url' => Configuration::get('BLOCKSOCIAL_RSS'),
+				'youtube_url' => Configuration::get('BLOCKSOCIAL_YOUTUBE'),
+				'google_plus_url' => Configuration::get('BLOCKSOCIAL_GOOGLE_PLUS'),
+				'pinterest_url' => Configuration::get('BLOCKSOCIAL_PINTEREST'),
+				'vimeo_url' => Configuration::get('BLOCKSOCIAL_VIMEO'),
+				'instagram_url' => Configuration::get('BLOCKSOCIAL_INSTAGRAM'),
+			));
+
+		return $this->display(__FILE__, 'blocksocial.tpl', $this->getCacheId());
+	}
+	
 	public function renderForm()
 	{
 		$fields_form = array(
